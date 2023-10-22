6023327f1b9249b17a3db35c9157597e222a7a4f
blackBuzo2601 <elianpro58@gmail.com>
2023-10-21 18:26:46 -0700

    Reinicio del codigo

--- app.js
+++ app.js
@@ -2,45 +2,27 @@
 //Elaborado por Buzo Zamora Elian
 //21 de Octubre de 2023, Ensenada B.C
 
+/*  usamos el modulo fs de Node.js
+    con require ('fs) se esta importando el modulo 
+    para acceder a todas las funciones y metodos de fs. */
+const fs = require ('fs'); 
+/*readFile es un método de fs.
+UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
+(err, data) son las variables que usamos como parámetro de nuestro callBack.
+err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
+data: se utiliza para almacenar los datos o el resultado de la operacion
+*/
+fs.readFile('sqlkeywords.txt','utf8', (err, data) => {
+
 var query="SELECT edad correo,sueldo FROM usuarios WHERE nombre='juan, pedro'";
-                            //NOTA: Si nombre= y 'juan tienen un espacio entre ellos,
-                            //la consola imprime los valores separados correctamente.
-                            //ejemplo: nombre=, 'juan, 
-                            //Pero si están como nombre='juan, los imprime raro pues
-                            //y es lo que me falta corregir
 
 
 
-//INICIALIZACION DE VARIABLES
-var querySpliteado=query.split(" "); //separar por espacios el Query
-var letra=""; 
-var palabraActual="";
-var subtexto=""; 
-var bandera=0;
-const caracteresDiferentes=",=" //Otros caracters que va a evaluar
 
-//QuerySpliteado: es un arreglo con todos los elementos de Query separados por el " "  bien.
-for(let i=0;i<querySpliteado.length;i++){ //Bucle For General
 
-    bandera=0; //reiniciar bandera.
-    palabraActual=querySpliteado[i]; //Trabajar con cada palabra.
 
-    for(let k=0;k<palabraActual.length;k++){ //bucle for que evalúa cada letra de palabraActual
-        letra=palabraActual[k];
-        if(caracteresDiferentes.includes(letra)){
-            console.log(letra); //imprimir el "caracterDiferente"
-            subtexto=palabraActual.split(letra); //splitear la palabra en base al caracter
-            for(let e=0;e<subtexto.length;e++){ 
-                console.log(subtexto[e]); //imprimir elementos de palabra separados por caracter
-            }
-            bandera=1; //significa que si entró en el condicional if. 
+}); //fin del readFile
 
-        }//cierre condicional if
 
-    }//fin bucle for de letras de la palabra
 
-    if(bandera==0){ //si bandera vale 1, significa que ya se imprimió la palabra con el caracter especial
-        console.log(palabraActual);
-    }
 
-}//fin for general  
