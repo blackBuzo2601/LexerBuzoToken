07dcfa8ab8fd6389d19f8c88fd82abdc07d8ba4f
blackBuzo2601 <elianpro58@gmail.com>
2023-10-22 00:58:08 -0700

    Intento del algoritmo anterior recorriendo letra por letra del query

--- app.js
+++ app.js
@@ -13,12 +13,42 @@ err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
 data: se utiliza para almacenar los datos o el resultado de la operacion
 */
 fs.readFile('sqlkeywords.txt','utf8', (err, data) => {
+var query="SELECT   edad correo,sueldo FROM usuarios WHERE nombre='juan, pedro'";
+//variables necesarias
+var caracterVacio=" ";
+var letraActual="";
+var formarPalabra="";
+var caracteresDiferentes=",'=";
+var letraSiguiente="";
 
-var query="SELECT edad correo,sueldo FROM usuarios WHERE nombre='juan, pedro'";
+function evaluaEspacios(){
+    
+}
 
+//Bucle for GENERAL
+for(let i=0;i<query.length;i++){
+    letraActual=query[i];
+    formarPalabra=formarPalabra+letraActual; //va construyendo la palabra hasta encontrar un espacio
+    if(letraActual==caracterVacio){
+        letraSiguiente=query[i+1]; //evalua si el siguiente caracter tambien es un espacio vacio
+        while(letraSiguiente==caracterVacio){
+            letraSiguiente=query[i+1];
+        }
+        
 
+        //console.log(formarPalabra);
+        //formarPalabra="";
+    }
+    
+}
 
-
+/*
+var variable=0;
+do{
+    variable=variable+10;
+    console.log("prueba");
+}while(variable<=100)
+*/
 
 
 }); //fin del readFile
