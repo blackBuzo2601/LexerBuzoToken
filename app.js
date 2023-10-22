f00b5ef9ef78aaefa0e54d34c23491079f6f5545
blackBuzo2601 <elianpro58@gmail.com>
2023-10-22 14:18:26 -0700

    Verifica que si está contando los espacios vacios bien

--- app.js
+++ app.js
@@ -5,56 +5,41 @@
 /*  usamos el modulo fs de Node.js
     con require ('fs) se esta importando el modulo 
     para acceder a todas las funciones y metodos de fs. */
-const fs = require ('fs'); 
-/*readFile es un método de fs.
-UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
-(err, data) son las variables que usamos como parámetro de nuestro callBack.
-err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
-data: se utiliza para almacenar los datos o el resultado de la operacion
-*/
-fs.readFile('sqlkeywords.txt','utf8', (err, data) => {
-var query="SELECT edad correo,sueldo FROM usuarios WHERE nombre='juan, pedro'";
-//variables necesarias
-var caracterVacio=" ";
-var letraActual="";
-var formarPalabra="";
-var caracteresDiferentes=",'=";
-var letraExtra="";
-var letraExtraPosicion=0;
-var letraActualPosicion=0;
-//Bucle for GENERAL
-for(let i=0;i<query.length;i++){
-    letraActual=query[i]; //almacena la letra
-    letraActualPosicion=i; //almacena la posicion
-    console.log("Letra: "+letraActual+" y posicion: "+letraActualPosicion);
-
-    formarPalabra=formarPalabra+letraActual; //va construyendo la palabra hasta encontrar un espacio
-    if(letraActual==caracterVacio){
-        console.log(formarPalabra);
-        console.log("espacio detectado en el caracter "+letraActualPosicion);
-        formarPalabra="";
-
-        //pasar a la siguiente letra a ver si es vacia
-        letraActual=query[letraActualPosicion+1]; //almacena la letra SIGUIENTE
-        letraActual=query.indexOf(letraActual);//almacena la posicion de letra SIGUIENTE
+    const fs = require ('fs'); 
+    /*readFile es un método de fs.
+    UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
+    (err, data) son las variables que usamos como parámetro de nuestro callBack.
+    err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
+    data: se utiliza para almacenar los datos o el resultado de la operacion
+    */
+    fs.readFile('sqlkeywords.txt','utf8', (err, data) => {
+    var query="SELECT  edad   correo,sueldo    FROM     usuarios      WHERE nombre='juan, pedro'";
+    //variables necesarias
+    var caracterVacio=" ";
+    var letraActual="";
+    var formarPalabra="";
+    var caracteresDiferentes=",'=";
+    var letraExtra="";
+    var letraExtraPosicion=0;
+    var letraActualPosicion=0;
+    var contadorEspacios=0;
+    //Bucle for GENERAL
+    for(let i=0;i<query.length;i++){
+        letraActual=query[i]; //almacena la letra
+        letraActualPosicion=i; //almacena la posicion
+        console.log("Letra: "+letraActual+" y posicion: "+letraActualPosicion);
+    
+        formarPalabra=formarPalabra+letraActual; //va construyendo la palabra hasta encontrar un espacio
         if(letraActual==caracterVacio){
-            letraActualPosicion=letraActualPosicion+1;
-            letraActual=query[letraActualPosicion];
-            while(letraActual==caracterVacio){
-                letraActualPosicion=letraActualPosicion+1;
-                letraActual=query[letraActualPosicion]; //almacena la letra SIGUIENTE
-                console.log("Espacio detectado en el caracter: "+letraActualPosicion);
-            }
-        }
+            console.log(formarPalabra);
+            console.log("espacio detectado en el caracter "+letraActualPosicion);
+            contadorEspacios++;
+            formarPalabra="";
+        }//fin del while
         
-    }//fin del while
-        
-        
-}//fin bucle for general
-
-
-}); //fin del readFile
-
-
-
+            
+    }//fin bucle for general
+    console.log("el total de espacios es: "+contadorEspacios);
+    
+    }); //fin del readFile
 
