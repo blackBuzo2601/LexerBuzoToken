ff4ea37aaf0f7f02a8416065a787f8115b6fd46b
blackBuzo2601 <elianpro58@gmail.com>
2023-10-22 16:40:09 -0700

    Ahora hace lo que se supone que deberia hacer originalmente la tarae anterior, que es simplemente que imprima las palabras en renglones diferentes y tambien si hay un caracter especial que lo imprima en una linea diferente y si lo hace

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Programa probado con Node JS 16.17.1
 //Elaborado por Buzo Zamora Elian
 //21 de Octubre de 2023, Ensenada B.C
+//NOTA 1: Estoy intentando hacer el mismo lexer anterior, pero esta vez evaluando caracter por caracter
+//e intentare que esta vez imprima bien las palabras sin saltos de linea adicionales. Osea bien.
 
 /*  usamos el modulo fs de Node.js
     con require ('fs) se esta importando el modulo 
@@ -13,38 +15,52 @@
     data: se utiliza para almacenar los datos o el resultado de la operacion
     */
     fs.readFile('sqlkeywords.txt','utf8', (err, data) => {
-    var query="SELECT        edad            correo,sueldo            FROM         usuarios           WHERE nombre='juan, pedro'";
+    var query="'SELECT edad correo,sueldo FROM usuarios WHERE nombre='juan, pedro'";
     //variables necesarias
     var caracterVacio=" ";
     var letraActual="";
     var formarPalabra="";
     var caracteresDiferentes=",'=";
 
-    var letraActualPosicion=0;
-    var contadorEspacios=0;
-
     //Bucle for GENERAL
     for(let i=0;i<query.length;i++){
         letraActual=query[i]; //almacena la letra
-        letraActualPosicion=i; //almacena la posicion
-    
-        if(letraActual!=caracterVacio){ //primero evalua si el caracter es diferente de vacio
-            formarPalabra=formarPalabra+letraActual; //va construyendo la palabra hasta encontrar un espacio
-        }
         
-        if(letraActual==caracterVacio){
-            if(formarPalabra==""){
-                formarPalabra="";
+        if(letraActual!=caracterVacio){ //primero evalua si el caracter SI es un caracter
+
+            if(caracteresDiferentes.includes(letraActual)){ //evalua si es un caracterEspecial
+                if(formarPalabra==""){
+                    formarPalabra="";
+                }else{
+                    console.log(formarPalabra);
+                    console.log(letraActual);
+                    formarPalabra="";
+                }   
             }else{
+                formarPalabra=formarPalabra+letraActual; //va construyendo la palabra hasta encontrar un espacio
+            }
+        }//fin primer condicional
+        
+
+        if(letraActual==caracterVacio){//después evalua si el caracter es vacio o " "
+            if(formarPalabra==""){ 
+                formarPalabra="";
+            }else{ //esto se corre si formarPalabra tiene puras letras
                 console.log(formarPalabra);
                 formarPalabra="";
-            }
+            }    
+        }//fin segundo condicional
             
-        }//fin del if
-            
-
     }//fin bucle for general
-  
+
+    if(query[query.length-1]!=" "){ //evalua si el ultimo caracter del query es diferente de vacio. Para imprimir la palabra formada
+        console.log(formarPalabra);
+    }
     
-    }); //fin del readFile
+
+
+
+
+
+}); //fin del readFile
 
